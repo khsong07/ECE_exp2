75f8b0f476469e25d5a6e42ad963214621747dbf
khsong07 <134723272+khsong07@users.noreply.github.com>
2023-11-06 09:34:46 +0900

    add report code

---
+++ week6/N00_seg_counter/N00_seg_counter.srcs/sources_1/new/oneshot.v
@@ -0,0 +1,18 @@
+module oneshot(clk,rst,btn,btn_trig);
+
+input clk,rst,btn;
+reg btn_reg;
+output reg btn_trig;
+ 
+always @(negedge rst or posedge clk)begin
+    if(!rst) begin
+        btn_reg<=0;
+        btn_trig<=0;
+    end
+    else begin
+        btn_reg<=btn;
+        btn_trig<=btn&~btn_reg;
+    end 
+end
+
+endmodule
